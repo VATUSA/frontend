995b6b6bb2ab85628f12c9b94be346c3c8250868
Rahul Parkar <rahul.a.parkar@gmail.com>
2017-08-31 18:48:09 +0100

    Fixed usage of sass in component and sass loader

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -161,13 +161,13 @@ module.exports = {
           // In production, we use a plugin to extract that CSS to a file, but
           // in development "style" loader enables hot editing of CSS.
           {
-            test: /\.css$/,
+            test: /\.(s|)css$/,
             use: [
               require.resolve('style-loader'),
               {
                 loader: require.resolve('css-loader'),
                 options: {
-                  importLoaders: 1,
+                  importLoaders: 2,
                 },
               },
               {
@@ -191,6 +191,9 @@ module.exports = {
                   ],
                 },
               },
+              {
+                loader: require.resolve('sass-loader'),
+              }
             ],
           },
           // "file" loader makes sure those assets get served by WebpackDevServer.
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Container, Image } from 'semantic-ui-react';
-import './Header.scss';
+import style from './Header.scss';
 import logo from './logo-dark.png';
 // import headerImage from './head.jpg';
 
 
 const Header = () => (
   <div>
-    <Container fluid className="head">
+    <Container fluid className={style.head}>
       <Image src={logo} className="logo" />
     </Container>
   </div>
