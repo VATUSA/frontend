c7a128875685d3cfc845197d0f05fd72c9f1671f
Rahul Parkar <rahul.a.parkar@gmail.com>
2017-09-01 21:50:33 +0100

    Clean up un-necessary files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Immutable from 'immutable';
 
-import './index.css';
 import './semantic/semantic.min.css';
 
 import App from './App';
