32fb8d7fdb3c7cd4c18206ffee27b03bf7e40fa8
Aaron Osher <aaron@oshers.com>
2017-09-06 04:12:42 +0100

    Fixed Header background image reference and added links to navbar.

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -20,11 +20,15 @@ export default class NavBar extends React.Component {
             <Dropdown.Menu>
               <Dropdown.Item
                 text="Albuquerque ARTCC"
-                value="http://www.zabartcc.org"
+                as="a"
+                target="_blank"
+                href="http://www.zabartcc.org"
               />
               <Dropdown.Item
                 text="Anchorage ARTCC"
-                value="http://www.vzanartcc.net"
+                as="a"
+                target="_blank"
+                href="http://www.vzanartcc.net"
               />
               <Dropdown.Item
                 text="Atlanta ARTCC"
@@ -32,47 +36,69 @@ export default class NavBar extends React.Component {
               />
               <Dropdown.Item
                 text="Boston ARTCC"
-                value="http://www.bvartcc.com"
+                as="a"
+                target="_blank"
+                href="http://www.bvartcc.com"
               />
               <Dropdown.Item
                 text="Chicago ARTCC"
-                value="http://www.zauartcc.org"
+                as="a"
+                target="_blank"
+                href="http://www.zauartcc.org"
               />
               <Dropdown.Item
                 text="Cleveland ARTCC"
-                value="http://www.zobartcc.com"
+                as="a"
+                target="_blank"
+                href="http://www.zobartcc.com"
               />
               <Dropdown.Item
                 text="Denver ARTCC"
-                value="http://www.denartcc.or"
+                as="a"
+                target="_blank"
+                href="http://www.denartcc.org"
               />
               <Dropdown.Item
                 text="Fort Worth ARTCC"
-                value="http://www.zfwartcc.net"
+                as="a"
+                target="_blank"
+                href="http://www.zfwartcc.net"
               />
               <Dropdown.Item
                 text="Honolulu CF"
-                value="http://www.hcfartcc.net"
+                as="a"
+                target="_blank"
+                href="http://www.hcfartcc.net"
               />
               <Dropdown.Item
                 text="Houston ARTCC"
-                value="http://www.zhuartcc.org"
+                as="a"
+                target="_blank"
+                href="http://www.zhuartcc.org"
               />
               <Dropdown.Item
                 text="Indianapolis ARTCC"
-                value="http://www.zidartcc.org"
+                as="a"
+                target="_blank"
+                href="http://www.zidartcc.org"
               />
               <Dropdown.Item
                 text="Jacksonville ARTCC"
-                value="http://www.zjxartcc.org"
+                as="a"
+                target="_blank"
+                href="http://www.zjxartcc.org"
               />
               <Dropdown.Item
                 text="Kansas City ARTCC"
-                value="http://www.kcartcc.com"
+                as="a"
+                target="_blank"
+                href="http://www.kcartcc.com"
               />
               <Dropdown.Item
                 text="Los Angeles ARTCC"
-                value="http://www.laartcc.org"
+                as="a"
+                target="_blank"
+                href="http://www.laartcc.org"
               />
               <Dropdown.Item
                 text="Memphis ARTCC"
@@ -80,15 +106,21 @@ export default class NavBar extends React.Component {
               />
               <Dropdown.Item
                 text="Miami ARTCC"
-                value="http://www.zmaartcc.net"
+                as="a"
+                target="_blank"
+                href="http://www.zmaartcc.net"
               />
               <Dropdown.Item
                 text="Minneapolis ARTCC"
-                value="http://www.minniecenter.org"
+                as="a"
+                target="_blank"
+                href="http://www.minniecenter.org"
               />
               <Dropdown.Item
                 text="New York ARTCC"
-                value="http://www.nyartcc.org"
+                as="a"
+                target="_blank"
+                href="http://www.nyartcc.org"
               />
               <Dropdown.Item
                 text="Oakland ARTCC"
@@ -96,15 +128,21 @@ export default class NavBar extends React.Component {
               />
               <Dropdown.Item
                 text="Salt Lake City ARTCC"
-                value="http://www.zlcartcc.com"
+                as="a"
+                target="_blank"
+                href="http://www.zlcartcc.com"
               />
               <Dropdown.Item
                 text="Seattle ARTCC"
-                value="http://www.zseartcc.org"
+                as="a"
+                target="_blank"
+                href="http://www.zseartcc.org"
               />
               <Dropdown.Item
                 text="Washington D.C. ARTCC"
-                value="http://www.vzdc.org"
+                as="a"
+                target="_blank"
+                href="http://www.vzdc.org"
               />
             </Dropdown.Menu>
           </Dropdown>
@@ -143,68 +181,118 @@ export default class NavBar extends React.Component {
           <Dropdown
             item
             text="Pilot Tools"
-            scrolling
           >
             <Dropdown.Menu>
               <Dropdown.Item
                 text="Getting Started"
+                as="a"
+                target="_blank"
+                href="https://www.vatsim.net/pilots/getting-started"
               />
               <Dropdown.Item
                 text="Events Calendar"
+                as="a"
+                target="_blank"
+                href="https://www.vatusa.net/forums/?action=calendar"
               />
               <Dropdown.Item
                 text="Training"
+                as="a"
+                target="_blank"
+                href="http://www.vatsim.net/pilots/training"
               />
               <Dropdown.Item
                 text="Virtual Airlines"
+                as="a"
+                target="_blank"
+                href="http://www.vatsim.net/pilots/virtual-airlines"
               />
               <Dropdown.Item
                 text="Charts"
+                as="a"
+                target="_blank"
+                href="http://www.aircharts.org/"
               />
               <Dropdown.Item
                 text="Routes"
+                as="a"
+                target="_blank"
+                href="http://www.flightaware.com/statistics/ifr-route/"
               />
               <Dropdown.Item
                 text="VATSIM Stats/Tracking"
+                as="a"
+                target="_blank"
+                href="http://stats.vatsim.net/"
               />
-              <Dropdown.Divider />
-              <Dropdown.Header
-                content="Weather"
-              />
-              <Dropdown.Menu
-                scrolling
+              <Dropdown
+                item
+                simple
+                text="Weather"
               >
-                <Dropdown.Item
-                  text="METARs"
-                />
-                <Dropdown.Item
-                  text="TAFs"
-                />
-                <Dropdown.Item
-                  text="PIREPs"
-                />
-                <Dropdown.Item
-                  text="Baloon Sounding"
-                />
-                <Dropdown.Item
-                  text="Winds Aloft"
-                />
-                <Dropdown.Item
-                  text="Graphical Forcasts"
-                />
-                <Dropdown.Item
-                  text="Prog Charts"
-                />
-                <Dropdown.Item
-                  text="Satellite Imagery"
-                />
-                <Dropdown.Item
-                  text="RADAR"
-                />
-                <Dropdown.Item
-                  text="ASOS/AWOS Stations"
-                />
-              </Dropdown.Menu>
+                <Dropdown.Menu>
+                  <Dropdown.Item
+                    text="METARs"
+                    as="a"
+                    target="_blank"
+                    href=""
+                  />
+                  <Dropdown.Item
+                    text="TAFs"
+                    as="a"
+                    target="_blank"
+                    href=""
+                  />
+                  <Dropdown.Item
+                    text="PIREPs"
+                    as="a"
+                    target="_blank"
+                    href=""
+                  />
+                  <Dropdown.Item
+                    text="Baloon Sounding"
+                    as="a"
+                    target="_blank"
+                    href=""
+                  />
+                  <Dropdown.Item
+                    text="Winds Aloft"
+                    as="a"
+                    target="_blank"
+                    href=""
+                  />
+                  <Dropdown.Item
+                    text="Graphical Forcasts"
+                    as="a"
+                    target="_blank"
+                    href=""
+                  />
+                  <Dropdown.Item
+                    text="Prog Charts"
+                    as="a"
+                    target="_blank"
+                    href=""
+                  />
+                  <Dropdown.Item
+                    text="Satellite Imagery"
+                    as="a"
+                    target="_blank"
+                    href=""
+                  />
+                  <Dropdown.Item
+                    text="RADAR"
+                    as="a"
+                    target="_blank"
+                    href=""
+                  />
+                  <Dropdown.Item
+                    text="ASOS/AWOS Stations"
+                    as="a"
+                    target="_blank"
+                    href=""
+                  />
+                </Dropdown.Menu>
+              </Dropdown>
             </Dropdown.Menu>
           </Dropdown>
           <Menu.Menu
