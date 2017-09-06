7a2d47649df8cc678e1871bb2eb731350150a42a
Aaron Osher <aaron@oshers.com>
2017-09-06 17:21:28 +0100

    Fixed UserBlob position issue

--- src/components/UserBlob/UserBlob.js
+++ src/components/UserBlob/UserBlob.js
@@ -4,12 +4,14 @@ import { Icon } from 'semantic-ui-react';
 export default class UserBlob extends React.Component {
   render() {
     return (
-      <div className="right wb-usr">
-        <span className="grab">User Blob</span><br />
-        <small>
-          <Icon name="user" />799999 - <Icon name="trophy" />C1 (Controller)<br />
-          <Icon name="star" />Staff - VATUSA
-        </small>
+      <div className="wb-usr-wrapper">
+        <div className="wb-usr">
+          <span className="grab">User Blob</span><br />
+          <small>
+            <Icon name="user" />799999 &nbsp; — &nbsp; <Icon name="trophy" />C1 (Controller)<br />
+            <Icon name="star" />Staff - VATUSA
+          </small>
+        </div>
       </div>
     );
   }
