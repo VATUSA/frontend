cd8df405359cef514b29286484f8592d13a66121
Aaron Osher <aaron@oshers.com>
2017-09-09 22:04:14 +0100

    Refactor TMUItem -- split single and multisecotr return into own functions

--- src/components/TMU/TMUItem.js
+++ src/components/TMU/TMUItem.js
@@ -2,19 +2,27 @@ import React, { Component, PropTypes } from 'react';
 import { Dropdown } from 'semantic-ui-react';
 
 class TMUItem extends Component {
+  static singleSector(dropdownTitle) {
+    return <Dropdown.Item text={dropdownTitle} />;
+  }
+
+  static multipleSectors(dropdownTitle, facility) {
+    return (
+      <Dropdown item text={dropdownTitle}>
+        <Dropdown.Menu>
+          {facility.map(sector => <Dropdown.Item key={sector.ident} text={`${sector.ident} - ${sector.name}`} />)}
+        </Dropdown.Menu>
+      </Dropdown>
+    );
+  }
+
   renderItem() {
     const { facility } = this.props;
     const dropdownTitle = `${facility[0].ident} - ${facility[0].name}`;
     if (facility.length === 1) {
-      return <Dropdown.Item text={dropdownTitle} />;
+      return TMUItem.singleSector(dropdownTitle);
     } else if (facility.length > 1) {
-      return (
-        <Dropdown item text={dropdownTitle}>
-          <Dropdown.Menu>
-            {facility.map(sector => <Dropdown.Item key={sector.ident} text={`${sector.ident} - ${sector.name}`} />)}
-          </Dropdown.Menu>
-        </Dropdown>
-      );
+      return TMUItem.multipleSectors(dropdownTitle, facility);
     }
     return null;
   }
