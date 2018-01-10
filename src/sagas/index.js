e326c99a55e9568ac152bfdc43cf9b0b7e69796d
Rahul Parkar <rahul.a.parkar@gmail.com>
2018-01-10 20:26:33 +0000

    Add event saga to root saga

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,5 +1,8 @@
-import { all } from 'redux-saga/effects';
+import { all, spawn } from 'redux-saga/effects';
+import eventSaga from './events';
 
 export default function* rootSaga() {
-  yield all([]);
+  yield all([
+    spawn(eventSaga),
+  ]);
 }
