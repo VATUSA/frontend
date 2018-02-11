06c516b989c5f7984513d2f70d148aac3eb3d7c3
Rahul Parkar <rahul.a.parkar@gmail.com>
2018-02-11 18:20:45 +0000

    Update references to take advantage of new aliases

--- src/components/EventsList/index.js
+++ src/components/EventsList/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Card, Feed } from 'semantic-ui-react';
-import { fetchEvents } from '../../ducks/events';
+import { fetchEvents } from 'ducks/events';
 
 class EventsList extends React.Component {
   componentDidMount() {
--- src/components/NewsList/index.js
+++ src/components/NewsList/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Card, Feed } from 'semantic-ui-react';
-import { fetchNews } from '../../ducks/news';
+import { fetchNews } from 'ducks/news';
 
 class NewsList extends React.Component {
   componentDidMount() {
