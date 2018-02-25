49b5bc08e1f6dafb255e165e4e727ffd418c79ea
Rahul Parkar <rahul.a.parkar@gmail.com>
2018-02-25 19:36:20 +0000

    Add login button behaviour

--- src/components/MainMenu/index.js
+++ src/components/MainMenu/index.js
@@ -2,21 +2,29 @@ import React from 'react';
 import { Container, Image, Menu, Button } from 'semantic-ui-react';
 import logo from '../../static/logo-light.png'; // Light preferred, dark usable
 
-export default () => (
-  <Container>
-    <Menu.Item><Image size="small" src={logo} /></Menu.Item>
-    <Menu.Item as="a" active>Home</Menu.Item>
-    <Menu.Item as="a">Facilities</Menu.Item>
-    <Menu.Item as="a">Forums</Menu.Item>
-    <Menu.Item as="a">Division Info</Menu.Item>
-    <Menu.Item as="a">Pilot Tools</Menu.Item>
-    <Menu.Item as="a">Join Us</Menu.Item>
-    <Menu.Item as="a">TMU Maps</Menu.Item>
-    <Menu.Item as="a">Support</Menu.Item>
-    <Menu.Menu position="right">
-      <Menu.Item className="item">
-        <Button as="a" primary>Log in</Button>
-      </Menu.Item>
-    </Menu.Menu>
-  </Container>
-);
+export default class MainMenu extends React.PureComponent {
+  onLoginButtonClick = () => {
+    window.location.href = 'https://login.vatusa.net';
+  }
+
+  render() {
+    return (
+      <Container>
+        <Menu.Item><Image size="small" src={logo} /></Menu.Item>
+        <Menu.Item as="a" active>Home</Menu.Item>
+        <Menu.Item as="a">Facilities</Menu.Item>
+        <Menu.Item as="a">Forums</Menu.Item>
+        <Menu.Item as="a">Division Info</Menu.Item>
+        <Menu.Item as="a">Pilot Tools</Menu.Item>
+        <Menu.Item as="a">Join Us</Menu.Item>
+        <Menu.Item as="a">TMU Maps</Menu.Item>
+        <Menu.Item as="a">Support</Menu.Item>
+        <Menu.Menu position="right">
+          <Menu.Item className="item">
+            <Button as="a" primary onClick={this.onLoginButtonClick}>Log in</Button>
+          </Menu.Item>
+        </Menu.Menu>
+      </Container>
+    );
+  }
+}
