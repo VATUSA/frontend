c371e79b8fe7b5f0c6249bd77aa85019e3457865
Rahul Parkar <rahul.a.parkar@gmail.com>
2018-03-05 00:36:00 +0000

    Call info as well, because why not.

--- src/sagas/auth.js
+++ src/sagas/auth.js
@@ -4,7 +4,11 @@ import request from 'utils/request';
 export default function* fetchToken() {
   try {
     const response = yield call(request.get, '/v2/auth/token');
-    yield window.sessionStorage.setItem(response.data.token);
+    yield window.sessionStorage.setItem('token', response.data.token);
+
+    yield call(request.get, '/v2/auth/info');
+
+    yield window.sessionStorage.setItem('info', response.data);
   } catch (e) {
     // TODO: Do something on failure maybe?
   }
